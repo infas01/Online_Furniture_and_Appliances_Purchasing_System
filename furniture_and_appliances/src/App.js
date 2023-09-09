4f4a7060c1450c3dc979fcc73347d48f316bd50e
Infas_NM <infas1002@gmail.com>
2023-09-09 13:34:06 +0530

    first commit

---
+++ furniture_and_appliances/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./nav";
+import { Button } from "@material-tailwind/react"; 
+
+const App = () => 
+{
+  return(
+    <>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
+
